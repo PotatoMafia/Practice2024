77206e3a976d1995032d24f6ded5f3adbcce5335
akuhach <01184770@pw.edu.pl>
2024-07-11 12:06:21 +0200

    Inter process communication(toList (working version with Collection))

--- microservices/inventory-service/src/main/java/com/programmingtechie/inventory_service/service/InventoryService.java
+++ microservices/inventory-service/src/main/java/com/programmingtechie/inventory_service/service/InventoryService.java
@@ -1,7 +1,8 @@
 package com.programmingtechie.inventory_service.service;
+
 import com.programmingtechie.inventory_service.repository.InventoryRepository;
+import com.programmingtechie.inventory_service.dto.*;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,10 +15,17 @@ import java.util.List;
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
-@Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
+
+    @Transactional(readOnly = true)
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
         log.info("Checking Inventory");
-         return inventoryRepository.findBySkuCode(skuCode).isPresent();
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0)
+                            .build()
+                ).toList();
 
     }
 }
--- microservices/order-service/src/main/java/com/programmingtechie/order_service/service/OrderService.java
+++ microservices/order-service/src/main/java/com/programmingtechie/order_service/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.programmingtechie.order_service.service;
 
 
+import com.programmingtechie.order_service.dto.InventoryResponse;
 import com.programmingtechie.order_service.dto.OrderLineItemsDto;
 import com.programmingtechie.order_service.dto.OrderRequest;
 import com.programmingtechie.order_service.model.Order;
@@ -9,24 +10,50 @@ import com.programmingtechie.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class OrderService {
-   private final OrderRepository orderRepository;
+    private final OrderRepository orderRepository;
+    private final WebClient webClient;
+
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
                 .stream()
                 .map(this::mapToDo)
-                .toList();
+                .collect(Collectors.toList());
 
         order.setOrderLineItemsList(orderLineItems);
+
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .collect(Collectors.toList());
+        /*
+         * Calling of The Inventory Service and place order if product is
+         * in stock
+         * */
+        InventoryResponse[] inventoryResponsArray = webClient.get()
+                .uri("http://localhost:8082/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponsArray).allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
         orderRepository.save(order);
     }
 

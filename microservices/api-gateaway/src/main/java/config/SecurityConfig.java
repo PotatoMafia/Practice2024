cc65785a2e249eda6442c9d609af668343faeba8
akuhach <01184770@pw.edu.pl>
2024-07-21 20:33:39 +0200

    Keycloak full

--- microservices/api-gateaway/src/main/java/config/SecurityConfig.java
+++ microservices/api-gateaway/src/main/java/config/SecurityConfig.java
@@ -1,3 +1,5 @@
+
+/*docker run -p 8181:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:25.0.2 start-dev*/
 package config;
 
 import org.springframework.context.annotation.Bean;
@@ -6,6 +8,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
+
 
 @Configuration
 @EnableWebFluxSecurity

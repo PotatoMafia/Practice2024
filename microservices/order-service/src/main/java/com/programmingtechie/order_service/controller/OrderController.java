caf69146ce142b25f3ac961e48bcf758912ac02f
akuhach <01184770@pw.edu.pl>
2024-07-22 21:06:34 +0200

    Implementation ot the Circuit Breaker

--- microservices/order-service/src/main/java/com/programmingtechie/order_service/controller/OrderController.java
+++ microservices/order-service/src/main/java/com/programmingtechie/order_service/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.programmingtechie.order_service.controller;
 
 import com.programmingtechie.order_service.dto.OrderRequest;
 import com.programmingtechie.order_service.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,8 +17,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return "Oops, sth went wrong, pls order later";
     }
 }

0e4c63aa534d1477d8b517cba18087c48171b5ff
akuhach <01184770@pw.edu.pl>
2024-07-23 21:16:09 +0200

    Implementation of the Circuit Breaker

--- microservices/order-service/src/main/java/com/programmingtechie/order_service/controller/OrderController.java
+++ microservices/order-service/src/main/java/com/programmingtechie/order_service/controller/OrderController.java
@@ -4,13 +4,18 @@ package com.programmingtechie.order_service.controller;
 import com.programmingtechie.order_service.dto.OrderRequest;
 import com.programmingtechie.order_service.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.concurrent.CompletableFuture;
+
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@TimeLimiter(name = "inventor")
 public class OrderController {
 
     private final OrderService orderService;
@@ -18,12 +23,13 @@ public class OrderController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
-    public String placeOrder(@RequestBody OrderRequest orderRequest) {
-        orderService.placeOrder(orderRequest);
-        return "Order Placed Successfully";
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        return CompletableFuture.supplyAsync(()->orderService.placeOrder(orderRequest));
     }
 
-    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
-        return "Oops, sth went wrong, pls order later";
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(() ->"Oops, sth went wrong, pls order later");
     }
 }
